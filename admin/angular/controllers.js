f027324ba10969819f229e28b42854969f925824
andreusfreeman <andriy.natsyk@gmail.com>
2016-06-08 01:57:59 +0300

    added table things

--- admin/angular/controllers.js
+++ admin/angular/controllers.js
@@ -2,10 +2,9 @@ app.controller('indexCtrl', function($scope){
   console.log('index');
 });
 app.controller('thingsCtrl', function($scope, $http){
-  $scope.test = 'Hello world!';
-  $http.get('http://127.0.0.1:8080/query/category.js?' + Math.random(100000000))
+  $http.get('http://127.0.0.1:8080/query/things.js?' + Math.random(100000000))
   .success(function(result){
-    console.log(result[0].id);
+    $scope.things = result;
   })
 });
 app.controller('addThingsCtrl', function($scope){
